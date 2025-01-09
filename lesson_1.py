c3ea8a5a7626ba364350d566c94f140d4defd67a
VladimiAni <toprussia72@gmail.com>
2025-01-09 18:39:59 +0300

    Create lesson_1.py

---
+++ lesson_1.py
@@ -0,0 +1,21 @@
+#NUMBER1
+from math import *
+R=int(input())
+L=round(2*pi*R, 2)
+S=round(pi*R**2, 2)
+print(L, S)
+
+#NUMBER2
+import math 
+x = 10
+y = 55
+x, y = y, x
+print(x)
+print(y)
+
+#NUMBER3
+from math import *
+g = 9.81
+L=int(input())
+T=round(2*pi*(L/g)**1/2 , 2)
+print(T)
